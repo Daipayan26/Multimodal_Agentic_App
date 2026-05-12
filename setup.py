9c7d232a65ebc75df980eb8adda36bdc65e6dcac
Daipayan26 <daipayan.roy26@gmail.com>
2026-05-12 21:54:24 +0530

    requirement paclkage installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="multimodal_chatbot",
+    version="0.1.0",
+    author="Daipayan Roy",
+    author_email="droy@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
